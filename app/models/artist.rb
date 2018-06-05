8c2b7c7a59419e3aaa449b044304642d405a1725
Fiona Lapham <fionadlapham@gmail.com>
2018-06-05 11:29:15 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    artist.song.first.genre
   end
 
   def song_count
