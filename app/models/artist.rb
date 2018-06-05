9c3a869fbdb126982b56fd1aa16434c654064c3d
Fiona Lapham <fionadlapham@gmail.com>
2018-06-05 11:38:06 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -16,9 +16,11 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-    binding.pry
     genres = self.songs.map do |song|
-      song.genre
+        song.genre
+      end
+      genres.uniq.count
   end
-  genres.uniq.count
+
+
 end
