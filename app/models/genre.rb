cb3adf683fb08db0f04e338fac4be2ce09c7f50d
Fiona Lapham <fionadlapham@gmail.com>
2018-06-05 11:41:44 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -17,5 +17,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.songs.map do |song|
+      song.artist.name
   end
 end
